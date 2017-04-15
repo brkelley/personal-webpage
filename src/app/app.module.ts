9a7542e9dba0cab3b8b6f38c5bf8542a2e8b3315
Brendan Kelley <brendan.bebo.kelley@gmail.com>
2017-04-15 19:34:41 -0400

    Added Angular Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BiographyComponent } from './biography/biography.component';
+import { ResumeComponent } from './resume/resume.component';
+
+const appRoutes: Routes = [
+    { path: 'home', component: HomepageComponent },
+    { path: 'resume', component: ResumeComponent },
+    { path: 'biography', component: BiographyComponent },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    { path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderNavComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderNavComponent,
+        HomepageComponent,
+        BiographyComponent,
+        ResumeComponent
+    ],
+    imports: [
+        RouterModule.forRoot(appRoutes),
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/header-nav/header-nav.component.ts
+++ src/app/header-nav/header-nav.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header-nav',
-  templateUrl: './header-nav.component.html',
-  styleUrls: ['./header-nav.component.scss']
+      selector: 'app-header-nav',
+      templateUrl: './header-nav.component.html',
+      styleUrls: ['./header-nav.component.scss']
 })
 export class HeaderNavComponent implements OnInit {
 
-  displayName: string;
-  tabs: Array<string>;
+      displayName: string;
+      tabs: Array<string>;
+      active: string;
 
-  constructor() {
-    this.displayName = 'Brendan Kelley';
-    this.tabs = ['GitHub', 'About Me', 'Resume', 'Projects'];
-  }
+    constructor() {
+        this.displayName = 'Brendan Kelley';
+        this.tabs = ['About Me', 'Resume', 'Projects'];
+    }
 
-  ngOnInit() {
+    ngOnInit() {
 
-  }
+    }
+
 
 }

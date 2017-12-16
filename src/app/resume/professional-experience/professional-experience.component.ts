abea7b6f8c6f7d6e677540579b2034d0e1007b83
Brendan <brendan.bebo.kelley@gmail.com>
2017-12-16 16:24:21 -0500

    finished resume

--- src/app/resume/professional-experience/professional-experience.component.ts
+++ src/app/resume/professional-experience/professional-experience.component.ts
@@ -5,19 +5,7 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 @Component({
   selector: 'professional-experience',
   templateUrl: './professional-experience.component.html',
-  styleUrls: ['./professional-experience.component.scss'],
-  animations: [
-    trigger('experienceDetails', [
-      transition(':enter', [
-        style({transform: 'translateX(100%)', opacity: 0}),
-        animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
-      ]),
-      transition(':leave', [
-        style({transform: 'translateX(0)', opacity: 1}),
-        animate('500ms', style({transform: 'translateX(100%)', opacity: 0}))
-      ])
-    ])
-  ]
+  styleUrls: ['./professional-experience.component.scss']
 })
 export class ProfessionalExperienceComponent implements OnInit {
 
